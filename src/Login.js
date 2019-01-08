83b524b2f9b4029ee276778e5305d628807b68b9
Simon <sisihamilton@hotmail.com>
2019-01-08 18:35:07 -0500

    created login component

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+
+    render() {
+        return(
+            <div>Login</div>
+        );
+    }
+}
+
+export default Login;
