121b6f37d4629c013724c5bbef8719b3fdc05d49
Greg Yaniw <greg@Gregs-MacBook-Air.local>
2019-04-14 20:05:17 -0400

    adding jobs container

---
+++ src/components/JobsContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import JobTile from './JobTile';
+
+class JobsContainer extends Component {
+    
+    render() {
+        return (
+            <div>
+                <JobTile />
+            </div>
+        );
+    }
+}
+
+export default JobsContainer;
